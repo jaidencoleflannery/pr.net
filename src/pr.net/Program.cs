f7a912d6da20d396a52bf3da52ad24ff3581f7ee
Cole Flannery <jaidencoleflannery@gmail.com>
2026-03-06 00:32:29 -0600

    logging | setup logging.

--- src/pr.net/Program.cs
+++ src/pr.net/Program.cs
@@ -1,16 +1,24 @@
+using Serilog;
 using pr.net.Models;
 using pr.net.Services;
-using static pr.net.Services.RequestEngine;
 
 public class Program {
     public static void Main(string[] args) {
         var builder = WebApplication.CreateBuilder(args);
+        builder.Host.UseSerilog(
+            (ctx, config) => 
+                config
+                    .ReadFrom.Configuration(ctx.Configuration)
+                    .WriteTo.Console()
+                    .WriteTo.File("logs/pr-.txt", rollingInterval: RollingInterval.Day)
+        );
+
+        builder.Services.AddSingleton<RequestEngine>();
+
         var app = builder.Build();
         
-        var _requestEngine = new RequestEngine();
-
         app.MapGet("/", () => "Server is running.");
-        app.MapGet("/pullrequestcreated", (ILogger<RequestEngine> logger, PullRequestDto request) => _requestEngine.ProcessNewPullRequest(logger, request));
+        app.MapPost("/pullrequestcreated", (ILogger<RequestEngine> logger, RequestEngine engine, PullRequestDto request) => engine.ProcessNewPullRequest(logger, request));
 
         app.Run();
     }
