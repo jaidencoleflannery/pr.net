6dd1485fd9b7858ba79a5aef2a35aa7413172506
Cole Flannery <jaidencoleflannery@gmail.com>
2026-03-14 00:05:19 -0500

    tokens | sync.

--- src/pr.net/Services/Tokens/TokenService/TokenService.cs
+++ src/pr.net/Services/Tokens/TokenService/TokenService.cs
@@ -2,11 +2,11 @@ namespace pr.net.Services.Tokens;
 
 public class TokenService(ITokenProvider provider) : ITokenService {
 
-    private Dictionary<Token, ICachedToken> _tokens = new Dictionary<string, ICachedToken>();
+    private Dictionary<Token, ICachedToken> _tokens = new Dictionary<Token, ICachedToken>();
 
     ValueTask<string> GetRepoTokenAsync() {
        if(_tokens.TryGetValue(Token.PR_NET_REPO_TOKEN, out var token)) {
-           return GetString(token); 
+           return Tokens.GetString(token); 
         }; 
     }
 
--- src/pr.net/Services/Tokens/Tokens.cs
+++ src/pr.net/Services/Tokens/Tokens.cs
@@ -2,14 +2,15 @@ namespace pr.net.Services.Tokens;
 
 public enum Token { PR_NET_REPO_TOKEN, PR_NET_CHAT_TOKEN }
 
-public static class TokenMap {
+public static class Tokens { 
+
     // since we know what Tokens holds, this gives us O(1) lookup time instead of waiting for tostring to lookup the name
     private static Dictionary<Token, string> _map = Enum.GetValues<Token>().ToDictionary(
         token => token,
         token => token.ToString()
     );
 
-    public static string GetString(this Token token) =>
+    public static string GetString(ICachedToken token) =>
         _map[token];
 
 }
