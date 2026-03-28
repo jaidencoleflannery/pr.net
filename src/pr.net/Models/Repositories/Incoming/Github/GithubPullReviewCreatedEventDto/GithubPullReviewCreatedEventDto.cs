790963f6d0d0d88689a6cf6c2d7aafaa54bdd41b
jaidencoleflannery <jaidencoleflannery@gmail.com>
2026-03-27 22:52:53 -0500

    modeling | fixing github models to reflect (filtered) version of github's event payload.

--- src/pr.net/Models/Repositories/Incoming/Github/GithubPullReviewCreatedEventDto/GithubPullReviewCreatedEventDto.cs
+++ src/pr.net/Models/Repositories/Incoming/Github/GithubPullReviewCreatedEventDto/GithubPullReviewCreatedEventDto.cs
@@ -5,8 +5,14 @@ namespace pr.net.Models.Github;
 
 public class GithubPullReviewCreatedEventDto : PullReviewCreatedEvent {
 
-    [JsonPropertyName("hook")]
-    public GithubHookDto Hook { get; set; } = new GithubHookDto();
+    [JsonPropertyName("action")]
+    public string Action { get; set; } = string.Empty;
+
+    [JsonPropertyName("number")]
+    public int Number { get; set; } = -1;
+
+    [JsonPropertyName("pull_request")]
+    public GithubPullRequestDto PullRequest { get; set; } = new GithubPullRequestDto();
 
     [JsonPropertyName("repository")]
     public GithubRepositoryDto Repository { get; set; } = new GithubRepositoryDto();
