007ddc2cba23c6e58e54f7ee71523df3953351c0
jaidencoleflannery <jaidencoleflannery@gmail.com>
2026-04-08 20:55:08 -0500

    amazon | implementing amazon integration.

---
+++ src/pr.net/Configurations/HostConfigurations/Modules/HostAmazonConfiguration/HostAmazonConfiguration.cs
@@ -0,0 +1,11 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace pr.net.Configurations.Host;
+
+public class HostAmazonConfiguration : IHostProviderConfiguration {
+ 
+    [Required]
+    [ConfigurationKeyName("SecretsManagerPath")] 
+    public string? SecretsManagerPath { get => field; init; }
+
+}
--- src/pr.net/Models/Enums/HostProviders.cs
+++ src/pr.net/Models/Enums/HostProviders.cs
@@ -21,7 +21,7 @@ public static class HostProviders {
 
     public enum HostProvider {
         Amazon, 
-        Local,
+        Environment,
         None
     }
 
